d7b8813a81baf7a4ac3ffc3e5b0c77f9781a12a1
Phil Boyd <phil.boyd@live.com>
2020-04-23 14:16:31 -0400

    lesson 59 complete

---
+++ Academind/react-the-complete-guide-incl-redux/module-04/lesson-057/src/App.js
@@ -0,0 +1,80 @@
+import React, { Component } from 'react';
+import './App.css';
+import Person from './Person/Person';
+
+class App extends Component {
+    state = {
+        persons: [
+            { id: '1', name: 'Phil', age: 53 },
+            { id: '2', name: 'Clara', age: 57, hobby: 'IFM' },
+            { id: '3', name: 'Branden', age: 20, hobby: 'Computer Games' },
+        ],
+        otherState: 'some other value',
+        showPersons: false,
+    };
+
+    nameChangedHandler = (event) => {
+        this.setState({
+            persons: [
+                { name: event.target.value, age: 53 },
+                { name: 'Clara', age: 27, hobby: 'IFM' },
+            ],
+        });
+    };
+
+    togglePersonsHandler = () => {
+        const show = this.state.showPersons;
+
+        this.setState({
+            showPersons: !show,
+        });
+    };
+
+    deletePersonHandler = (index) => {
+        // const persons = this.state.persons.slice();
+        const persons = [...this.state.persons];
+        persons.splice(index, 1);
+        this.setState({ persons: persons });
+    };
+
+    render() {
+        const style = {
+            backgroundColor: 'lightgreen',
+            font: 'inherit',
+            border: '2px dashed blue',
+            padding: '8px',
+            cursor: 'pointer',
+        };
+
+        let persons = null;
+        if (this.state.showPersons) {
+            persons = (
+                <div>
+                    {this.state.persons.map((person, index) => {
+                        return (
+                            <Person
+                                key={person.id}
+                                name={person.name}
+                                age={person.age}
+                                click={() => this.deletePersonHandler(index)}>
+                                My hobby is {person.hobby}
+                            </Person>
+                        );
+                    })}
+                </div>
+            );
+        }
+
+        return (
+            <div className='App'>
+                <h1>Hi, I'm a React App</h1>
+                <button style={style} onClick={this.togglePersonsHandler}>
+                    Toggle list
+                </button>
+                {persons}
+            </div>
+        );
+    }
+}
+
+export default App;
