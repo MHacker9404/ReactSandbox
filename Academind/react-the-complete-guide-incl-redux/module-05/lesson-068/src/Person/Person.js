63c8e3f524bb35e9e727653797643fc19da9b6ee
Phil Boyd <phil.boyd@live.com>
2020-04-23 20:41:13 -0400

    lesson 69 complete

--- Academind/react-the-complete-guide-incl-redux/module-05/lesson-068/src/Person/Person.js
+++ Academind/react-the-complete-guide-incl-redux/module-05/lesson-068/src/Person/Person.js
@@ -5,9 +5,14 @@ import Radium from 'radium';
 const person = (props) => {
     console.info(props);
 
-    //return <p>I'm {props.name} and I am { Math.floor(Math.random() * 50) } years old!</p>
+    const style = {
+        '@media (min-width: 500px)': {
+            width: '450px',
+        },
+    };
+
     return (
-        <div className='Person'>
+        <div className='Person' style={style}>
             <p>
                 I'm {props.name} and I am {props.age} years old!
             </p>
