e773d5f9d6b5f2efed2eea5a31a9af460070db15
Phil Boyd <phil.boyd@live.com>
2020-04-28 14:11:26 -0400

    module 08 - BurgerBuilder init

---
+++ Academind/react-the-complete-guide-incl-redux/module-08/burger-builder/src/App.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import './App.scss';
+// import logo from './logo.svg';
+// import Button from './Button/Button';
+
+function App() {
+    return (
+        <></>
+        // <div className="App">
+        //     <header className="App-header">
+        //         <img src={logo} className="App-logo" alt="logo" />
+        //         <p>
+        //             Edit <code>src/App.js</code> and save to reload.
+        //         </p>
+        //         <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
+        //             Learn React
+        //         </a>
+        //         <hr />
+        //         <button className="btn" onClick={() => alert('I am globally styled')}>
+        //             I am button 1 - Press Me
+        //         </button>
+        //         <Button />
+        //     </header>
+        // </div>
+    );
+}
+
+export default App;
