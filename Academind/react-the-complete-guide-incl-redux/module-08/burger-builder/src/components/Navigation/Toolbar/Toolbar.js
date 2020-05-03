81a738f6a1152d9ebc6a852353ebd5f77fd97be9
Phil Boyd <phil.boyd@live.com>
2020-05-03 15:49:21 -0400

    lesson 145 complete

--- Academind/react-the-complete-guide-incl-redux/module-08/burger-builder/src/components/Navigation/Toolbar/Toolbar.js
+++ Academind/react-the-complete-guide-incl-redux/module-08/burger-builder/src/components/Navigation/Toolbar/Toolbar.js
@@ -1,13 +1,16 @@
 import React from 'react';
 import css from './Toolbar.module.scss';
 import Logo from '../../Logo/Logo';
+import NavigationItems from '../NavigationItems/NavigationItems';
 
 const toolbar = (props) => {
     return (
         <header className={css.Toolbar}>
             <div>MENU</div>
             <Logo />
-            <nav>LINKS</nav>
+            <nav>
+                <NavigationItems />
+            </nav>
         </header>
     );
 };
