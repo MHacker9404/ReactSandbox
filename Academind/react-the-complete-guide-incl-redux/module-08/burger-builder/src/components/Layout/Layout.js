d116b1a20c479884c7a457e24d7548999b46b7c2
Phil Boyd <phil.boyd@live.com>
2020-05-03 17:39:53 -0400

    lesson 149 complete

--- Academind/react-the-complete-guide-incl-redux/module-08/burger-builder/src/components/Layout/Layout.js
+++ Academind/react-the-complete-guide-incl-redux/module-08/burger-builder/src/components/Layout/Layout.js
@@ -1,20 +1,25 @@
-import React from 'react';
+import React, { Component } from 'react';
 import css from './Layout.module.scss';
 import Toolbar from '../Navigation/Toolbar/Toolbar';
 import SideDrawer from '../Navigation/SideDrawer/SideDrawer';
 
-const layout = (props) => {
-    return (
+class Layout extends Component {
+    state = {
+        showSideDrawer: true,
+    };
+    sideDrawerClosedHandler = () => {
+        this.setState({ showSideDrawer: false });
+    };
+
+    render = () => (
         <>
-            <div>
-                <Toolbar />
-                <SideDrawer />
-                <p>backdrop</p>
-            </div>
+            <Toolbar />
+            <SideDrawer open={this.state.showSideDrawer} closed={this.sideDrawerClosedHandler} />
+            <p>backdrop</p>
             <hr />
-            <main className={css.content}>{props.children}</main>
+            <main className={css.content}>{this.props.children}</main>
         </>
     );
-};
+}
 
-export default layout;
+export default Layout;
--- Academind/react-the-complete-guide-incl-redux/module-08/burger-builder/src/components/Navigation/SideDrawer/SideDrawer.js
+++ Academind/react-the-complete-guide-incl-redux/module-08/burger-builder/src/components/Navigation/SideDrawer/SideDrawer.js
@@ -2,18 +2,24 @@ import React from 'react';
 import css from './SideDrawer.module.scss';
 import Logo from '../../Logo/Logo';
 import NavigationItems from '../NavigationItems/NavigationItems';
+import Backdrop from '../../UI/Backdrop/Backdrop';
 
 const sideDrawer = (props) => {
+    const classNames = [css.SideDrawer, props.open ? css.Open : css.Close].join(' ');
+
     return (
-        <div className={css.SideDrawer}>
-            {/* <Logo height="11%" /> */}
-            <div className={css.Logo}>
-                <Logo />
+        <>
+            <Backdrop show={props.open} clicked={props.closed} />
+            <div className={classNames}>
+                {/* <Logo height="11%" /> */}
+                <div className={css.Logo}>
+                    <Logo />
+                </div>
+                <nav>
+                    <NavigationItems />
+                </nav>
             </div>
-            <nav>
-                <NavigationItems />
-            </nav>
-        </div>
+        </>
     );
 };
 
